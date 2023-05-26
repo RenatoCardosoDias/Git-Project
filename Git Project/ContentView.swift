c11f189d01914805a4ae2b22bd3ab18658b1b3d7
Renato Dias <renatocardosodias@icloud.com>
2023-05-26 11:37:30 -0300

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Renato on 26/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPresented = false
+
+
+    var body: some View {
+        NavigationView{
+            Button("Show Modal"){
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented){
+                Text("This is the modal screen")
+            } //end .sheet
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Renato on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
